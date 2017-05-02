1d6830cfe642926b20af57303f06cf9449f2b9bc
Dave Chaston <dave@caffeinateddave.com>
2017-05-03 00:39:15 +0100

    Add passwords

--- web.rb
+++ web.rb
@@ -10,6 +10,21 @@ Dotenv.load
 include Mongo
 
 use Rack::Logger
+use Rack::Session::Cookie, :key => 'rack.session',
+  #                         :domain => ENV['DOMAIN'],
+                           :path => '/',
+                           :expire_after => 60 * 60, # 1 hr. 
+                           :secret => 'bmJSq9SZexKAeSv8'
+
+register do
+  def auth (type)
+    condition do
+      if !session[:valid_user]
+        redirect '/'
+      end
+    end
+  end
+end
 
 helpers do
   def logger
@@ -25,26 +40,45 @@ before do
   end
 end
 
-get '/?' do
+get '/home?', :auth => :any do
+  session[:login_attempted] = nil
   first = $db[:settings].find.limit(1).first
   @files = first[:images]
   @got = first[:lastCheck]
   erb :index
 end
 
-post '/update' do
+get '/' do
+  erb :login
+end
+
+post '/' do
+  if params[:password] == ENV['password']
+    session[:valid_user] = true
+    redirect '/home'
+  else 
+    session[:login_attempted] = true
+    redirect '/'
+  end
+end
+
+post '/update', :auth => :any  do
   if params['key'] != "images" 
     $db[:settings].find_one_and_update({}, 
       {"$set" => {params['key'] => params['value']}},
       {:return_document => :after}  
     )
   end
-  redirect to('/')
+  redirect to('/home')
 end
 
-post '/imageupload' do
+post '/imageupload', :auth => :any  do
   file = params[:file][:tempfile]
   name = params[:name]
+
+  if file == nil or name == ""
+    halt 403, "Invalid input"
+  end
 
   grid_file = Mongo::Grid::File.new(
     file.read,
@@ -57,7 +91,7 @@ post '/imageupload' do
     {"$push" => {:images => name}},
     {:return_document => :after} 
   )
-  redirect to('/')
+  redirect to('/home')
 end
 
 get '/images/:name' do |name|
@@ -69,7 +103,7 @@ get '/images/:name' do |name|
   send_file out, type: 'image/bmp', disposition: 'inline'
 end
 
-get '/deleteImage/:name' do |name|
+get '/deleteImage/:name', :auth => :any  do |name|
   settings = $db[:settings].find.limit(1).first
   images = settings[:images]
   images.delete(params[:name])
@@ -82,7 +116,7 @@ get '/deleteImage/:name' do |name|
   file = fs.find_one(:filename => name)
   fs.delete(file.id)
 
-  redirect to('/')
+  redirect to('/home')
 end
 
 get '/settings' do
